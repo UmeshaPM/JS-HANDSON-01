9547a8cf2f8a28dbe15fea0f8b62bccc5b361dc1
Umesha  PM <umeshpm4@gmail.com>
2022-07-06 12:16:58 +0530

    JS-HANDSON-01

---
+++ 5.Last_Digit.js
@@ -0,0 +1,8 @@
+//Q-05. Find the last digit of a 4 digit number
+
+//You are provided a four digit number N only. Your task is to print out the last digit of that number.
+
+const Last_Digit = (n) => {
+    return((n%10));
+ };
+ console.log(Last_Digit(4567));
---
+++ 6.Find_the_Reminder.js
@@ -0,0 +1,8 @@
+//Q-06. Find the remainder
+
+//You are provided with two numbers A and B where A as divisor and B as dividend.Your task is find the remainder.
+
+const Find_the_remainder = (a,b) => {
+    return ((b%a));
+};
+console.log(Find_the_remainder(2,9));
